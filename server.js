ea4f0a6955ca7d004c4a91a3f44a4e33080b3093
James Skipp <jamesskipp@gmail.com>
2017-11-08 21:59:35 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs')
+  .use(express.static('public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.ip} ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+    console.log(log);
+  });
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+  res.render('jamesskipp.hbs', {
+    content: fs.readFileSync('html/about.html')
+  });
+});
+
+app.get('/skills', (req, res) => {
+  res.render('jamesskipp.hbs', {
+    content: fs.readFileSync('html/skills.html')
+  });
+});
+
+app.get('/resume', (req, res) => {
+  res.render('jamesskipp.hbs', {
+    content: fs.readFileSync('html/resume.html')
+  });
+});
+
+app.get('/blog', (req, res) => {
+  res.render('jamesskipp.hbs', {
+    content: '<div><p>Blog Coming Soon!</p></div>'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('jamesskipp.hbs', {
+    content: '<div><p>Projects Coming Soon!</p></div>'
+  });
+});
+
+app.listen(3000, () => {
+  var now = new Date().toString();
+  var log = `${now}: Started server on port 3000`;
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+    console.log(log);
+  });
+});
